27d033d9771050ba56342feb541391c6c4d2870e
Bladimir Gerson Parra Bermudez | IP Proyectos y Soluciones <ipproyectossoluciones@gmail.com>
2026-05-17 13:44:42 -0500

    feat(nvim): complete IDE stack (completion, dap, telescope, git, formatting)

--- nvim/lua/plugins/completion/init.lua
+++ nvim/lua/plugins/completion/init.lua
@@ -1,9 +1,5 @@
 return {
 
-  -- =====================================
-  -- CMP Core
-  -- =====================================
-
   {
     "hrsh7th/nvim-cmp",
 
@@ -21,14 +17,12 @@ return {
     },
 
     config = function()
-
       local cmp = require("cmp")
       local luasnip = require("luasnip")
 
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
-
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
@@ -36,43 +30,17 @@ return {
         },
 
         mapping = cmp.mapping.preset.insert({
-
-          ["<C-Space>"] = cmp.mapping.complete(),
-
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
-
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
+          ["<C-Space>"] = cmp.mapping.complete(),
         }),
 
-        sources = cmp.config.sources({
+        sources = {
           { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
           { name = "buffer" },
           { name = "path" },
-        }),
-
+          { name = "luasnip" },
+        },
       })
-
     end,
   },
 }
