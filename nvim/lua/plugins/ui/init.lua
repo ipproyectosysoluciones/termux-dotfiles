c9640bb089c62ac9c15bada151724dd06833a92e
Bladimir Gerson Parra Bermudez | IP Proyectos y Soluciones <ipproyectossoluciones@gmail.com>
2026-05-17 10:17:51 -0500

    add telescope ui module

--- nvim/lua/plugins/ui/init.lua
+++ nvim/lua/plugins/ui/init.lua
@@ -1,3 +1,17 @@
 return {
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
 
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>" },
+    },
+  },
 }
